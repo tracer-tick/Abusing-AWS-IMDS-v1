06cb8bf0b5d152356b4437177af223d4f19969a9
Ryan Tick <ryan.g.tick@gmail.com>
2020-04-20 01:17:46 -0500

    Updating alb settings

--- terraform/modules/application-load-balancer/main.tf
+++ terraform/modules/application-load-balancer/main.tf
@@ -1,11 +1,10 @@
 resource "aws_lb" "alb" {
-  name               = var.name
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [var.security_group_id]
-  subnets            = var.public_subnet_ids
-
-  enable_deletion_protection = true
+  name                        = var.name
+  internal                    = false
+  load_balancer_type          = "application"
+  security_groups             = [var.security_group_id]
+  subnets                     = var.public_subnet_ids
+  enable_deletion_protection  = false
 
   tags = {
     Name  = var.name,
